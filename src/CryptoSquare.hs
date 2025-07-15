fc65b629815b1c99ae495e0403e6d1174a87e48b
Dan Kjeldstrøm Hansen <dankh@live.dk>
2025-07-15 10:32:44 +0200

    solution

--- src/CryptoSquare.hs
+++ src/CryptoSquare.hs
@@ -1,4 +1,17 @@
 module CryptoSquare (encode) where
 
+import Data.Char (isAlphaNum, toLower)
+import Data.List (transpose)
+import GHC.Float (sqrtDouble)
+
 encode :: String -> String
-encode xs = error "You need to implement this function."
+encode xs = unwords . transpose . padTo r $ grouped r n
+  where
+    n = map toLower (filter isAlphaNum xs)
+    len = length n
+    r = ceiling $ sqrtDouble $ fromIntegral len
+
+    grouped _ [] = []
+    grouped i cs = take i cs : grouped i (drop i cs)
+
+    padTo x = map (\cs -> cs ++ replicate (x - length cs) ' ')
