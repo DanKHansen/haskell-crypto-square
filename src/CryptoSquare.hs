14399fd4744875c587b54141c24306622ab917a8
Dan Kjeldstrøm Hansen <dankh@live.dk>
2025-07-15 12:32:04 +0200

    refactor

--- src/CryptoSquare.hs
+++ src/CryptoSquare.hs
@@ -2,16 +2,14 @@ module CryptoSquare (encode) where
 
 import Data.Char (isAlphaNum, toLower)
 import Data.List (transpose)
+import Data.List.Split (chunksOf)
 import GHC.Float (sqrtDouble)
 
 encode :: String -> String
-encode xs = unwords . transpose . padTo r $ grouped r n
+encode xs = unwords . transpose . padTo r $ chunksOf r n
   where
     n = map toLower (filter isAlphaNum xs)
     len = length n
     r = ceiling $ sqrtDouble $ fromIntegral len
-
-    grouped _ [] = []
-    grouped i cs = take i cs : grouped i (drop i cs)
 
     padTo x = map (\cs -> cs ++ replicate (x - length cs) ' ')
